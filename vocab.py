a7102cb61df2e58d619facddc48bcd2357ba8251
Sarvesh Gulhane <49842473+SarveshGulhane@users.noreply.github.com>
2020-07-30 20:38:36 +0530

    PEP standard

--- vocab.py
+++ vocab.py
@@ -9,32 +9,32 @@ letters = list(string.ascii_lowercase)  # create list of alphabets
 spell = SpellChecker()
 
 # if user want list of words starts with any letter
-def ListofWords():
+def list_of_words():
     while True:
-        letterInput = input("\nEnter few starting letters :")
-        if letterInput != "/":
-            tempList = []
+        letter_input = input("\nEnter few starting letters :")
+        if letter_input != "/":
+            temp_list = []
             # used regular expresions to find words starting with given input
             for word in words:
-                if re.findall('^' + str(letterInput), word) != []:
-                    tempList.append(word)
-            print("Words:\n", tempList)
+                if re.findall('^' + str(letter_input), word) != []:
+                    temp_list.append(word)
+            print("Words:\n", temp_list)
         else:
             break
 
 # if user want spell check
-def SpellCheck():
+def spell_check():
     while True:
-        wordInput = input("\nEnter a word: ")
-        if wordInput != "/":
+        word_input = input("\nEnter a word: ")
+        if word_input != "/":
             # if input word is in list of words
-            if wordInput in words:
+            if word_input in words:
                 print("Correct spelling!!")
             else:
                 # if word is not in list of words, then use SpellChecker library
                 print("Incorrect spelling..")
-                print('Do you mean: ', spell.correction(wordInput))
-                print('Suggestions: ', spell.candidates(wordInput))
+                print('Do you mean: ', spell.correction(word_input))
+                print('Suggestions: ', spell.candidates(word_input))
         else:
             break
 
@@ -47,9 +47,9 @@ while True:
     What you want to do: '''))
 
     if choice == 1:
-        ListofWords()
+        list_of_words()
     if choice == 2:
-        SpellCheck()
+        spell_check()
     if choice == 3:
         break
     if choice > 3:
